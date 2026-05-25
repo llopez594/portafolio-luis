5d2fe768147fe0a45407b6d3f64af1b12942e0fc
Luis J. Lopez M. <luisjlopez594@gmail.com>
2026-05-25 00:15:08 -0400

    conexion backend produccion

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
 API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000'
-    : 'https://tu-backend.onrender.com',
+    : 'https://llopez594.alwaysdata.net/portafolio-backend',
 };
